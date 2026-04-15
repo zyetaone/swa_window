f831c3d6d3f05dbc2e9b423f2786cf583da4bb31
Rick <31343717+rdtect@users.noreply.github.com>
2026-04-16 05:17:10 +0530

    chore: drop stray refactor scripts

--- fix_mess2.py
+++
@@ -1,8 +0,0 @@
-with open('src/routes/playground/+page.svelte', 'r') as f:
-    text = f.read()
-
-text = text.replace('speed=cloudSpeed={pg.cloudSpeed}', 'speed={pg.cloudSpeed}')
-
-with open('src/routes/playground/+page.svelte', 'w') as f:
-    f.write(text)
-
